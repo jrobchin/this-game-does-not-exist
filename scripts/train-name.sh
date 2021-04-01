68702511d6e71704e784b31d73ec3cca78b0deb9
Jason Chin <jason.rob.chin@gmail.com>
2021-03-31 23:30:48 -0400

    add all and game training data

---
+++ scripts/train-name.sh
@@ -0,0 +1,11 @@
+python run_clm.py \
+--model_type "gpt2-medium" \
+--model_name_or_path "gpt2-medium" \
+--train_file "../data/training/name_train.txt" \
+--do_train \
+--validation_file "../data/training/name_val.txt" \
+--do_eval \
+--num_train_epochs 5 \
+--fp16 \
+--output_dir "../data/models/gpt2-name/" \
+--per_gpu_train_batch_size 1
